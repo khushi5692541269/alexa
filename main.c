58adf3d8d48ad4d030a15c3e71e3ec8bc47ed734
khushi5692541269 <jatinsharmarealme5@gmail.com>
2024-12-28 14:31:26 +0530

    Create main.c

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main();
+{
+printf("\nHAPPY NEW YEAR 2025");
+Printf("\n may your year be as amazing as you are");
+printf("\n may god bless you");
+printf("#");
+printf("enjoy your life");
+}
